18efa96bb96a0208e512b24cd549438a876eef14
Sergio Paucara <39333761+paukiss@users.noreply.github.com>
2021-04-04 12:40:20 -0400

    Add files via upload

--- mibase.sql
+++ mibase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2021 at 04:09 PM
+-- Generation Time: Apr 04, 2021 at 06:40 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -61,7 +61,21 @@ INSERT INTO `nota` (`pk_nota`, `ci`, `sigla`, `nota1`, `nota2`, `nota3`, `notaFi
 (17, 4685426, 'EST155', 90, 90, 96, 92),
 (18, 8546156, 'EST133', 68, 68, 68, 68),
 (19, 8546156, 'EST145', 51, 80, 76, 69),
-(20, 8546156, 'EST155', 80, 80, 86, 82);
+(20, 8546156, 'EST155', 80, 80, 86, 82),
+(21, 7894561, 'EST133', 65, 65, 65, 65),
+(22, 7894561, 'EST145', 70, 82, 70, 74),
+(23, 7894561, 'EST155', 51, 51, 51, 51),
+(24, 868686, 'INF111', 30, 51, 75, 52),
+(25, 868686, 'INF121', 30, 51, 75, 52),
+(26, 868686, 'INF131', 72, 72, 72, 72),
+(27, 868686, 'INF143', 80, 80, 86, 82),
+(28, 666555, 'MAT114', 72, 72, 72, 72),
+(29, 666555, 'MAT115', 30, 51, 75, 52),
+(30, 666555, 'MAT116', 90, 90, 96, 92),
+(31, 123456789, 'INF111', 100, 100, 100, 100),
+(32, 123456789, 'INF121', 90, 90, 90, 90),
+(33, 123456789, 'INF131', 80, 80, 80, 80),
+(34, 123456789, 'INF143', 51, 51, 51, 51);
 
 -- --------------------------------------------------------
 
@@ -81,12 +95,16 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`ci`, `nombreCompleto`, `fechaNacimiento`, `departamento`) VALUES
-(989596, 'Daniel Sanchez', '2001-12-25', 'LP'),
+(666555, 'Mateo Suarez', '1970-10-17', 'CB'),
+(868686, 'Lucas Loza', '1994-11-20', 'SC'),
+(989596, 'Daniel Sanchez', '2001-12-25', 'CB'),
 (4685426, 'Sandra Ramirez', '2000-10-17', 'CB'),
+(7894561, 'Pedro Perez', '1985-08-05', 'LP'),
 (8546156, 'Juana Vargas', '1999-11-20', 'SC'),
 (9566435, 'Karen Rocha', '2000-08-05', 'LP'),
 (10935816, 'Sergio Alejandro Paucara Saca', '2000-11-05', 'LP'),
-(12131415, 'Antonio Banderas', '1985-01-05', 'SC');
+(12131415, 'Antonio Banderas', '1985-01-05', 'SC'),
+(123456789, 'Silvestre el Gato', '1945-03-24', 'LP');
 
 -- --------------------------------------------------------
 
@@ -105,12 +123,16 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`ci`, `usuario`, `password`) VALUES
+(666555, 'mateo', '123456'),
+(868686, 'lucas', '123456'),
 (989596, 'daniel', '123456'),
 (4685426, 'sandra', '123456'),
+(7894561, 'pedro', '123456'),
 (8546156, 'juana', '123456'),
 (9566435, 'karen', '123456'),
 (10935816, 'sergio', '123456'),
-(12131415, 'antonio', '123456');
+(12131415, 'antonio', '123456'),
+(123456789, 'silvestre', '123456');
 
 --
 -- Indexes for dumped tables
@@ -142,7 +164,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `nota`
 --
 ALTER TABLE `nota`
-  MODIFY `pk_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `pk_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
